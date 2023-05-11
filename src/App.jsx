a004c835d87e934149d9e8633f4cdebb7669c6c1
Soulemanou Kunchiefuh Babanou <kunchiefuh@gmail.com>
2023-05-11 22:23:10 +0200

    added the nav_panel

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
-import './App.css';
+import React from 'react';
+import './App.css'
+import Sidebar from './components/sidebar';
+
 
 function App() {
   return (
-    <>
-    </>
-  );
+<div className="container">
+  <Sidebar />
+</div>
+  )
 }
 
 export default App;

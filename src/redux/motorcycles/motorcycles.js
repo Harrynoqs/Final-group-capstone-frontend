3cc894ea5d6a38148cf1599c6c63426e67e00d6a
Ellon M <mordecaimwema11@gmail.com>
2023-05-11 16:53:42 +0300

    matched api result to action parameters

--- src/redux/motorcycles/motorcycles.js
+++ src/redux/motorcycles/motorcycles.js
@@ -5,12 +5,18 @@ export const addMotorcycle = createAsyncThunk(
   'motorcycles/getMotorCycles',
   async (motorcycle) => {
     // values from Motorcycle model
-    const { name } = motorcycle;
+    const { name, description, imageUrl, price, modelYear, engineType, fuelType } = motorcycle;
     try {
       await axios.post(
-        '',
+        `${import.meta.env.VITE_API_ENDPOINT}/twowheelers/new`,
         {
           name,
+          description,
+          image_url: imageUrl,
+          price,
+          model_year: modelYear,
+          engine_type: engineType,
+          fuel_type: fuelType
         },
       );
       return motorcycle;

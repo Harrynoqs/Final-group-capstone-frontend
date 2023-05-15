e6e2f1c66acfb957b2bfae894856603cfc877764
Soulemanou Kunchiefuh Babanou <kunchiefuh@gmail.com>
2023-05-15 15:47:22 +0200

    fixed main liter error

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
+import { ThemeProvider } from '@material-tailwind/react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
-import { ThemeProvider } from "@material-tailwind/react";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>

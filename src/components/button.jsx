74d0a367cddf308f47662797b024e8da6635cd03
Soulemanou Kunchiefuh Babanou <kunchiefuh@gmail.com>
2023-05-15 15:47:37 +0200

    fixed btn liter error

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,5 +1,5 @@
-import { Button } from "@material-tailwind/react";
- 
+import { Button } from '@material-tailwind/react';
+
 export default function Example() {
   return <Button>Button</Button>;
 }

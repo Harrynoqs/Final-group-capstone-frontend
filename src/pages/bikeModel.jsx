233aa869a5cdadad7f2759ff54abfbf315d15c0f
Soulemanou Kunchiefuh Babanou <kunchiefuh@gmail.com>
2023-05-15 17:26:40 +0200

    added the bikedetails plus its properties

--- src/pages/bikeModel.jsx
+++ src/pages/bikeModel.jsx
@@ -1,13 +1,26 @@
 import React from 'react';
 // import ButtonX from '../components/button';
 import BikeSwiper from '../components/bikeSwiper';
-// import Bikedetails from '../components/BikeDetails';
+import Bikedetails from '../components/BikeDetails';
 
 export default function BikeModel() {
   return (
     <div className="flex flex-col items-center justify-center pt-5">
-      <BikeSwiper />
-      {/* <Bikedetails /> */}
+      {/* <BikeSwiper /> */}
+      
+      <Bikedetails
+  bikeData={{
+    name: 'SEH-BIKE 976',
+    price: 4356,
+    financeFee: 768,
+    purchaseFee: 2768,
+    totalAmount: 9768,
+    duration: '45Hr',
+    image: './images/black1.png',
+    configurationOptions: ['Option 1', 'Option 2', 'Option 3'],
+  }}
+/>
+
     </div>
   );
 }

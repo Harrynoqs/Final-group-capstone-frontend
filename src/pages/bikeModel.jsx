473e3ca2009646fb08e6eb16055d74308d676aeb
Soulemanou Kunchiefuh Babanou <kunchiefuh@gmail.com>
2023-05-15 15:48:31 +0200

    fixed bikemodel liter error

--- src/pages/bikeModel.jsx
+++ src/pages/bikeModel.jsx
@@ -1,12 +1,13 @@
-import React from "react";
-import ButtonX from "../components/button";
-import BikeSwiper from "../components/bikeSwiper";
-import Bikedetails from "../components/BikeDetails";
-export default function BikeModel(){
-  return(
-   <div className="flex flex-col items-center justify-center pt-5">
-   <BikeSwiper />
-   {/* <Bikedetails /> */}
-   </div>
+import React from 'react';
+// import ButtonX from '../components/button';
+import BikeSwiper from '../components/bikeSwiper';
+// import Bikedetails from '../components/BikeDetails';
+
+export default function BikeModel() {
+  return (
+    <div className="flex flex-col items-center justify-center pt-5">
+      <BikeSwiper />
+      {/* <Bikedetails /> */}
+    </div>
   );
 }

6427ea4a58b5b38deed148295b1f7199ad26bfca
Ellon M <mordecaimwema11@gmail.com>
2023-05-15 21:39:16 +0300

    store user data in browser to persist login

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,4 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
+import userReducer from './users/users';
 import motorcycleReducer from './motorcycles/motorcycles';
 import reservationReducer from './reservations/reservations';
 
@@ -6,7 +7,24 @@ const store = configureStore({
   reducer: {
     motorcycle: motorcycleReducer,
     reservation: reservationReducer,
+    user: userReducer
   },
 });
+
+const handleChange = () => {
+  let nextState = store.getState().user.user;
+  if (nextState) {
+    nextState = [...new Map(nextState.map((item) => (
+      [item.id, item]))).values()];
+    const serializedState = JSON.stringify(nextState);
+    localStorage.setItem('state', serializedState);
+  }
+}
+
+export const unsubscribe = store.subscribe(() => {
+  handleChange();
+});
+
+handleChange();
 
 export default store;

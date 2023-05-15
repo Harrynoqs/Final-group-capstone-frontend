519b1c3330422db2c5771ecae181d46b65aa603c
Soulemanou Kunchiefuh Babanou <kunchiefuh@gmail.com>
2023-05-15 14:53:46 +0200

    modified the resposiveness

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import './App.css'
+import BikeModel from './pages/bikeModel';
 import Sidebar from './components/sidebar';
-
-
-function App() {
+import { LoadingIcon } from './components/loadingIcon';
+ function App() {
   return (
-<div className="container">
-  <Sidebar />
-</div>
-  )
+    <div className="flex flex-col min-h-screen sm:flex-row">
+      <Sidebar />
+      <div className="flex-1 border-2 border-gray-200 border-dashed rounded-lg md:p-4 md:ml-64 dark:border-gray-700">
+        <Suspense fallback={<LoadingIcon />}>
+          <BikeModel />
+        </Suspense>
+      </div>
+    </div>
+  );
 }
-
-export default App;
+ export default App;

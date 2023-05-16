f1a1cd155bf0535b7ca7086a88d3565912e09505
Harrison Njuguna <noqsnoqs@gmail.com>
2023-05-16 15:36:57 +0300

    Modify app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,17 +7,18 @@ import BikeModel from './pages/bikeModel';
 import Sidebar from './components/sidebar';
 import LoadingIcon from './components/loadingIcon';
 import Login from './components/login';
+import Reservations from './components/reservations';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="flex flex-col min-h-screen sm:flex-row">
         <Sidebar />
-        <div className="flex-1 border-2 border-gray-200 border-dashed rounded-lg md:p-4 md:ml-64 dark:border-gray-700">
+        <div className="flex-1 border-2 border-gray-200 border-dashed rounded-lg md:p-4 md:ml-64 dark:border-gray-700 p-2">
           <Suspense fallback={<LoadingIcon />}>
             <Routes>
-              <Route index element={<BikeModel />} />
-              <Route path="/login" element={<Login />} />
+              <Route index element={<Reservations />} />
+              {/* <Route path="/login" element={<Login />} /> */}
             </Routes>
           </Suspense>
         </div>
